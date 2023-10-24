de770b9626f312cad28c47a219105bd2430ff35a
Habyeok <earnest_hb@naver.com>
2023-10-24 11:46:33 +0900

    수정

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      유후
+      배포를 진행하겠습니다. 11시 46분
     </main>
   )
 }
