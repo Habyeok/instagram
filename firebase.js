6bc13ba012643c3639a13568ec58db1cd17970dd
Habyeok <earnest_hb@naver.com>
2023-10-24 11:02:18 +0900

    setting

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTK7_iATJGpdN9GFzGGsJTZYOQ_JUf2IA",
+  authDomain: "instagram-87f68.firebaseapp.com",
+  projectId: "instagram-87f68",
+  storageBucket: "instagram-87f68.appspot.com",
+  messagingSenderId: "397323890679",
+  appId: "1:397323890679:web:15b70f8dc646225be048f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
